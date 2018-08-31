4340d63b96aa7804ecc2284fa770c40ea866f5f3
Leegeunhyeok <lghlove0509@naver.com>
2018-08-31 10:31:46 +0900

    데이터베이스 연동 예제 작성, 파이썬 예제 파일명 변경

---
+++ src/test/mysql test/pymysql_0.9.2.py
@@ -0,0 +1,22 @@
+# PyMysql 0.9.2
+
+import pymysql
+
+# DB 커넥션 생성
+conn = pymysql.connect(host="localhost", port=3306, user="root", password="1234", db="vote")
+
+# 커서 객체 (실제 쿼리는 커서에서 진행)
+cur = conn.cursor(pymysql.cursors.DictCursor)
+
+# 커서의 execute 메소드로 원하는 쿼리 실행
+cur.execute("SHOW TABLES")
+
+# 쿼리 실행 후 fetch
+result = cur.fetchall()
+
+# 쿼리 결과 출력
+print(result)
+
+# 사용 후 close()
+cur.close()
+conn.close()
