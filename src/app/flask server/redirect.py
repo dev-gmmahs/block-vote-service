1d3e74a6acde1bbd2d44fb020c1567657748c4a0
leegeunhyeok <lghlove0509@naver.com>
2018-10-27 17:54:30 +0900

    리다이렉트 URL 수정

--- src/app/flask server/redirect.py
+++ src/app/flask server/redirect.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def main(path):
-    return redirect("https://coidroid.com", code=302)
+    return redirect("https://www.coidroid.com", code=302)
 
 
 if __name__ == "__main__":
